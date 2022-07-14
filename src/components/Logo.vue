0570ad2ea5bf7817ac2fbe43b3e951118b61dfb3
7581058 <100559990+7581058@users.noreply.github.com>
2022-07-14 21:40:13 +0900

    Header Logo 구성, Fonts 설정

---
+++ src/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterLink to="/" class="logo">
+    <span>OMDbAPI</span>.COM
+  </RouterLink>
+</template>
+
+<style scoped lang="scss">
+@import "~/scss/main";
+  .logo {
+    font-family: 'Oswald', sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+    color: $black;
+    text-decoration: none;
+    &:hover {
+      color: $black;
+    }
+    span {
+      color: $primary;
+    }
+  }  
+</style>
