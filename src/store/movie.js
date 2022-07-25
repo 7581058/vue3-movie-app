2d07e7878aa30caa39ec5a3d2d3d392afe7fbd31
7581058 <100559990+7581058@users.noreply.github.com>
2022-07-25 21:18:23 +0900

    영화 검색 추가요청 구성

--- src/store/movie.js
+++ src/store/movie.js
@@ -17,7 +17,7 @@ export default {
     }
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '14c167f8'
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
@@ -25,6 +25,21 @@ export default {
       commit('updateState', {
         movies: Search
       })
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10) 
+      
+      if(pageLength > 1) {
+        for(let i = 2; i <= pageLength; i += 1) {
+          if ( i > (number / 10)) break
+          
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${i}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          }) 
+        }
+      }
     }
   }
 }
