964398fd81a60b6ae3fc7ed85958b09ab39a7020
7581058 <100559990+7581058@users.noreply.github.com>
2022-08-07 23:51:11 +0900

    About 페이지 구성, 이미지 로딩 애니메이션 적용

--- src/store/about.js
+++ src/store/about.js
@@ -5,7 +5,7 @@ export default {
     email: '7581058@gmail.com',
     git: 'https://github.com/7581058',
     phone: '+82-10-1234-5678',
-    
+    image: 'https://github.com/7581058/vue3-movie-app/blob/main/src/assets/G1.png?raw=true'
   })
   
 }
